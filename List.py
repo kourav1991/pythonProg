8287998d9f2b6aa57bb7229506e3b8b1a2e1fb27
ShivKumar Kourav <SKourav@slb.com>
2019-01-29 11:25:45 +0530

    Iniital draft

---
+++ List.py
@@ -0,0 +1,18 @@
+
+
+aList = [10,20,30,40,50]
+print("Element are :", aList)
+print("First Element :", aList[0])
+print("second Element :", aList[1])
+print("third Element :", aList[2])
+print("Forth Element :", aList[3])
+print("Fifth Element :", aList[4])
+
+aList[0] = 100
+print("After Modifying :" , aList)
+
+bList = ["unix",".Net","Oracle"]
+print ("Languages are :", bList)
+
+cList = [10,20,"abcd","a",40]
+print("Items are : " , cList)
